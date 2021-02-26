9250f7c664f3b3484146289532ab77097214ceb1
Tektronica <9472267+Tektronica@users.noreply.github.com>
2021-02-25 20:42:35 -0800

    + broke up dual_output_gui into serveral files + Fixed lows tied sequence. Currently requires relay strobing + Grid currently disabled in this commit since it was causing some users to experience the application freezing.

--- VisaClient.py
+++ VisaClient.py
@@ -1,10 +1,12 @@
-import visa
+import pyvisa as visa
 import re
 import time
 
 
 class VisaClient:
     def __init__(self, id):
+        self.okay = True
+
         try:
             self.rm = visa.ResourceManager()
             self.instr_info = id
@@ -18,60 +20,71 @@ class VisaClient:
                    "    See NI-VISA Installation:\n"
                    "        > https://pyvisa.readthedocs.io/en/1.8/getting_nivisa.html#getting-nivisa\n")
             print(msg)
+            self.okay = False
+
+        if self.okay:
+            self.connect()
+
+    def connect(self):
         for attempt in range(5):
+            self.okay = True
             try:
                 # TODO - verify this works as intended... Otherwise leave INSTR lines commented
                 # if mode is SOCKET:
                 if self.mode == 'SOCKET':
                     # SOCKET is a non-protocol raw TCP connection
-                    address = self.instr_info['ip_address']
+                    address = self.instr_info['address']
                     port = self.instr_info['port']
                     self.INSTR = self.rm.open_resource(f'TCPIP0::{address}::{port}::SOCKET', read_termination='\n')
 
                 # if mode is GPIB:
                 elif self.mode == 'GPIB':
-                    address = self.instr_info['gpib_address']
+                    address = self.instr_info['gpib']
                     self.INSTR = self.rm.open_resource(f'GPIB0::{address}::0::INSTR')
 
                 # if mode is INSTR:
                 elif self.mode == 'INSTR':
                     # INSTR is a VXI-11 protocol
-                    address = self.instr_info['ip_address']
+                    address = self.instr_info['address']
                     self.INSTR = self.rm.open_resource(f'TCPIP0::{address}::inst0::INSTR', read_termination='\n')
 
                 # if mode is SERIAL:
                 elif self.mode == 'SERIAL':
-                    address = self.instr_info['ip_address']
+                    address = self.instr_info['address']
                     self.INSTR = self.rm.open_resource(f'{address}')
                     self.INSTR.read_termination = '\n'
 
                 # TODO - http://lampx.tugraz.at/~hadley/num/ch9/python/9.2.php
                 # if mode is SERIAL:
                 elif self.mode == 'USB':
-                    address = self.instr_info['ip_address']
+                    address = self.instr_info['address']
                     self.INSTR = self.rm.open_resource(f'{address}', read_termination='\n')
 
-                # if mode is NIGHTHAWK:
-                elif self.mode == 'NIGHTHAWK':
-                    address = self.instr_info['ip_address']
-                    port = self.instr_info['port']
-                    self.INSTR = self.rm.open_resource(f'TCPIP::{address}::{port}::SOCKET', read_termination='>')
-                    self.read()
-
                 else:
-                    print('Failed to connect.')
-                # This re finds any double newline combos that might occur and then removes them. Makes for better print
-                print(re.sub(r'[\r\n|\r\n|\n]+', '', self.INSTR.query('*IDN?').split("\r")[0].lstrip()))
+                    print('No such mode.')
 
-                self.INSTR.timeout = self.timeout
+                # test communication to instrument by identifying instrument
+                idn = re.sub(r'[\r\n|\r\n|\n]+', '', self.INSTR.query('*IDN?').lstrip(' '))
+                print(f"[FOUND] {idn}")
 
-            except (visa.VisaIOError, Exception) as e:
+            except visa.VisaIOError:
                 # https://github.com/pyvisa/pyvisa-py/issues/146#issuecomment-453695057
                 print(f'[attempt {attempt + 1}/5] - retrying connection to instrument')
+                self.okay = False
             else:
                 break
+
+        if not self.okay:
+            self.InstrumentConnectionFailed(self.instr_info)
         else:
-            print('Invalid session handle. The resource might be closed.')
+            self.INSTR.timeout = self.timeout
+
+    def InstrumentConnectionFailed(self, info):
+        """Raised when attempted connection to instrument has timedout or is unreachable"""
+        if info['mode'] in ('SOCKET', 'SERIAL'):
+            print(f"\nCannot reach address {info['address']} over {info['mode']}. Connection timed out.")
+        else:
+            print(f"\nCannot reach address {info['gpib']} over {info['mode']}. Connection timed out.")
 
     def info(self):
         return self.instr_info
@@ -85,6 +98,7 @@ class VisaClient:
                 response = re.sub(r'[\r\n|\r\n|\n]+', '', self.INSTR.query('*IDN?').lstrip(' '))
         except visa.VisaIOError:
             print('Failed to connect to address.')
+            self.okay = False
         return response
 
     def write(self, cmd):
@@ -107,10 +121,20 @@ class VisaClient:
         return response
 
     def close(self):
-        self.INSTR.close()
+        try:
+            self.INSTR.close()
+        except AttributeError:
+            # If caught, 'VisaClient' object will have no attribute 'INSTR'. Occurs when instrument not initialized.
+            if self.instr_info['mode'] in ('SOCKET', 'SERIAL'):
+                print(f"instrument at address {self.instr_info['address']} could not be disconnected.")
+                print(f"instrument may not have been found.\n")
+            else:
+                print(f"instrument at address {self.instr_info['gpib']} could not be disconnected.")
 
 
 def main():
+    """ THIS IS A TEST METHOD FOR THE VISACLIENT CLASS"""
+
     f5560A_id = {'ip_address': '129.196.136.130', 'port': '3490', 'gpib_address': '', 'mode': 'NIGHTHAWK'}
     f5790A_id = {'ip_address': '', 'port': '', 'gpib_address': '6', 'mode': 'GPIB'}
     k34461A_id = {'ip_address': '10.205.92.63', 'port': '3490', 'gpib_address': '', 'mode': 'INSTR'}
